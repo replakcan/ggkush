d1ee47450a5d62a60f09c46b422e031320c735de
replakcan <alper.makcan@gmail.com>
2025-02-03 20:36:09 +0300

    Initial commit

---
+++ src/main/java/com/techinwork/ggkush/GgkushApplication.java
@@ -0,0 +1,13 @@
+package com.techinwork.ggkush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgkushApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgkushApplication.class, args);
+	}
+
+}
