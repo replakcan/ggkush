43b52c4d861fa896812a8c464d6f20684c32a11c
replakcan <alper.makcan@gmail.com>
2025-02-11 22:55:37 +0300

    Adds CrossOrigin annotation on tweet_controller

--- src/main/java/com/techinwork/ggkush/controller/TweetController.java
+++ src/main/java/com/techinwork/ggkush/controller/TweetController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173.com")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/tweets")
