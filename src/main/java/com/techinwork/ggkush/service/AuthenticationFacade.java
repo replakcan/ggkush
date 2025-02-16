157afe786a3bf343d939c985158a52e3db6ba747
replakcan <alper.makcan@gmail.com>
2025-02-17 00:29:27 +0300

    Gets authUser email with a new layer

---
+++ src/main/java/com/techinwork/ggkush/service/AuthenticationFacade.java
@@ -0,0 +1,15 @@
+package com.techinwork.ggkush.service;
+
+import com.techinwork.ggkush.repository.IAuthenticationFacade;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFacade implements IAuthenticationFacade {
+
+    @Override
+    public Authentication getAuthentication() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+}
