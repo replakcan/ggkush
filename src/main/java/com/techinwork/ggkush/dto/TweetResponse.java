7b40ab36d5384105456139e3b9034bf88d8d686b
replakcan <alper.makcan@gmail.com>
2025-03-03 23:08:52 +0300

    Adds commentsList to TweetResponse record

--- src/main/java/com/techinwork/ggkush/dto/TweetResponse.java
+++ src/main/java/com/techinwork/ggkush/dto/TweetResponse.java
@@ -1,6 +1,7 @@
 package com.techinwork.ggkush.dto;
 
 import java.util.Date;
+import java.util.List;
 
-public record TweetResponse(Long id, String text, int votes, String nickName, Date createdDate) {
+public record TweetResponse(Long id, String text, int votes, String nickName, Date createdDate, List<CommentResponse> comments) {
 }
